c63d712172583512ff13d78e8b99b2d50decf10f
karino <hogeika2@gmail.com>
2014-04-11 13:51:49 +0900

    Change to new app-id and add deploy script.

---
+++ jsonengine/misc_scripts/myupdate.sh
@@ -0,0 +1,2 @@
+# /Users/karino/Program/eclipse/plugins/com.google.appengine.eclipse.sdkbundle_1.7.2/appengine-java-sdk-1.7.2/bin/appcfg.sh update ../war
+/Users/karino/Documents/work/appengine-java-sdk-1.9.2/bin/appcfg.sh update ../war
